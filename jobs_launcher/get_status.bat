f2bbf5ab20b5b6a53252cd8980bf610bf3abca4b
Alekseevich Andrey <magister.demens@gmail.com>
2020-03-17 19:01:23 +0300

    Remove files

--- jobs_launcher/get_status.bat
+++
@@ -1,2 +0,0 @@
-set PATH=c:\python35\;c:\python35\scripts\;%PATH%
-python -c "import core.status_exporter; core.status_exporter.main('%1')"
--- jobs_launcher/install/readme.md
+++
@@ -1,3 +0,0 @@
-## TODO
-* installation script for OSX
-* pyautogui: https://pyautogui.readthedocs.io/en/latest/install.html
